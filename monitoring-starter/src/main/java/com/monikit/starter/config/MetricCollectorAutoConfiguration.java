4ce52a2a3382eb4befd7a9c206b6faf31d105483
RyuSangwon <rsw2@connectly.co.kr>
2025-02-24 16:13:37 +0900

    메트릭 컬렉터 ObjectProvider 적용

--- monitoring-starter/src/main/java/com/monikit/starter/config/MetricCollectorAutoConfiguration.java
+++ monitoring-starter/src/main/java/com/monikit/starter/config/MetricCollectorAutoConfiguration.java
@@ -1,22 +1,21 @@
 package com.monikit.starter.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 
 import com.monikit.core.MetricCollector;
 import com.monikit.starter.MoniKitMetricCollector;
 import com.monikit.starter.NoOpMetricCollector;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+
 
 /**
  * 사용자가 원하는 `MetricCollector`를 주입할 수 있도록 자동 설정하는 클래스.
