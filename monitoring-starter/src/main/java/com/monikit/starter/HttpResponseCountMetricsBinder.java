353812fb0394670324cfb000105c9224d7691fe9
RyuSangwon <rsw2@connectly.co.kr>
2025-02-25 17:42:48 +0900

    메트릭 컬렉터 고도화 및 메트릭필터 제거

--- monitoring-starter/src/main/java/com/monikit/starter/HttpResponseCountMetricsBinder.java
+++ monitoring-starter/src/main/java/com/monikit/starter/HttpResponseCountMetricsBinder.java
@@ -19,21 +19,8 @@ public class HttpResponseCountMetricsBinder implements MeterBinder {
     public void bindTo(MeterRegistry registry) {
         this.meterRegistry = registry;
 
-        // 기본적인 카운터를 미리 등록 (예시)
-        registerCounter("/default", "200");
     }
 
-    private void registerCounter(String path, String status) {
-        String counterKey = "http_response_count|" + path + "|" + status;
-
-        counterCache.computeIfAbsent(counterKey, key ->
-            Counter.builder("http_response_count")
-                .description("Total number of HTTP responses")
-                .tag("path", path)
-                .tag("status", status)
-                .register(meterRegistry)
-        );
-    }
 
     /**
      * 동적으로 Counter를 기록하는 메서드
